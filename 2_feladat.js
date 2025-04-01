636bed2575bda464fe3299825a1c210833aad985
BarnaCsaba_PG <barna.csaba@diak.szbi-pg.hu>
2025-04-01 10:13:55 +0200

    Update 2_feladat.js

--- 2_feladat.js
+++ 2_feladat.js
@@ -48,3 +48,137 @@ console.log("Paros elemek darabszama:" + paros.length);
 console.log("Paratlan elemek darabszama:" + paratlan.length);
 
 // d
+
+let hettel = false
+
+for (let i = 0; i < randomok.length; i++) {
+    if (randomok[i] % 7 == 0) {
+        hettel = true
+    }
+}
+
+if (hettel == true) {
+    console.log("Van hettel oszthato szam");
+} else {
+    console.log("Nincs hettel oszthato szam");
+}
+
+// e
+
+let nega = false
+
+for (let i = 0; i < randomok.length; i++) {
+    if (randomok[i-1] < 0 && 0 < randomok[i+1]) {
+        nega = true
+    }
+}
+    
+if (nega == true) {
+    console.log("Van olyan elem melynek mindkét szomszédja negatív");
+} else {
+    console.log("Nincs olyan elem melynek mindkét szomszédja negatív");
+}
+
+// f
+
+let nagyobb = false
+
+for (let i = 0; i < randomok.length; i++) {
+    if(randomok[i] > randomok[i-1] + randomok[i+1]) {
+        nagyobb = true
+    }
+}
+    
+if (nagyobb == true) {
+    console.log("Van olyan elem amely nagyobb a szomszédjai összegénél");
+} else {
+    console.log("Nincs olyan elem amely nagyobb a szomszédjai összegénél");
+}
+
+// g
+
+let isis = 0;
+
+for (let i = 0; i < randomok.length; i++) {
+    if (randomok[i] % 3 == 0 && !randomok[i] % 5 == 0) {
+    isis = randomok[i];
+    
+    }
+}
+console.log("Az utolsó 3-mal igen de 5-tel nem oszthato szam:" + isis);
+
+// h
+let db2 = 0;
+let vane = false;
+
+for (let i = 0; i < randomok.length; i++) 
+    {
+        db2 = 0;
+        for (let j = 0; j < randomok.length; j++)
+            {
+                if (randomok[i] == randomok[j])
+                    {
+                        db2++;
+                    }
+                if (db2 >= 3)
+                    {
+                        vane = true;
+                    }
+            }
+    }
+
+if (vane == true) {
+    console.log("Van olyan elem amely 3-szor is szerepel");
+} else {
+    console.log("Nincs olyan elem amely 3-szor is szerepel");
+}   
+
+// i
+
+let no = false;
+
+for (let i = 0; i < randomok.length; i++) {
+    if (randomok[i] == randomok[i+1] || randomok[i] == randomok[i-1]) {
+    no = true;
+    }
+    else {no = false}
+}
+
+if (no == true) {console.log("Van olyan elem amely mindkét szomszédja azonos vele")
+
+} else {console.log("Nincs ilyen elem")}
+
+
+// j
+let ige = [];
+for (let i = 0; i < randomok.length; i++) {
+    if (randomok[i] % 10 == 0)
+    {
+        ige.push(i);
+     }
+}
+
+console.log("10 többszöröseinek az indexei:" + ige);
+
+
+// k
+
+let yap = 0;
+for (let i = 0; i < randomok.length; i++) {
+    yap += randomok[i];
+
+
+}
+
+
+let atlag = yap / randomok.length;
+
+
+let alatti = [];
+for (let i = 0; i < randomok.length; i++) {
+    if (randomok[i] < atlag) {
+        alatti.push(randomok[i]);
+        
+    }
+}
+console.log("Az átlagnál kisebb dobások(" + alatti.length +"db):" + alatti);
